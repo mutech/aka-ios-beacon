bd822bc297c36129638ec855d20bc5a2510035c4
Michael Utech <michael.utech@aka-labs.com>
2015-03-23 17:17:44 +0100

    AKACommons: added theme support for UIView+AKAconstraintTools; AKAControls: add support for recursion in view hierarchy control creation, new composite control binding type,

--- AKACommons/Classes/UIView+AKAConstraintTools.h
+++ AKACommons/Classes/UIView+AKAConstraintTools.h
@@ -10,6 +10,197 @@
 
 @interface UIView (AKAConstraintTools)
 
-- (NSArray*)aka_removeConstraintsAffecting:(UIView*)view;
+/**
+ * Determines if self is (directly) affected by the specified constraint.
+ *
+ * @note that this only tests first- and secondItem of the constraint
+ * and thus does not attempt to perform any magic to find out if the constraint
+ * really affects the view.
+ *
+ * @param constraint the constraint to test
+ *
+ * @return YES if the constraint references the view, NO otherwise.
+ */
+- (BOOL)aka_isAffectedByConstraint:(NSLayoutConstraint*)constraint;
+
+/**
+ * Determines if self is the only view (directly) affected by the specified constraint.
+ *
+ * @note This only tests first- and secondItem of the constraint
+ * and thus does not attempt to perform any magic to find out if the constraint
+ * really affects the view.
+ *
+ * @note Only direct instances of NSLayoutConstraint are included. Intances of private
+ *       or custom constraint implementations are ignored.
+ *
+ * @param constraint the constraint to test
+ *
+ * @return YES if the constraint references no other view, NO otherwise.
+ */
+- (BOOL)aka_isTheOnlyViewDirectlyAffectedBy:(NSLayoutConstraint*)constraint;
+
+/**
+ * Gets an array containing all constraints which directly affect the specified view.
+ *
+ * @sa aka_isAffectedByConstraint:
+ *
+ * @param view the view
+ *
+ * @return all constraints affecting the specified view.
+ */
+- (NSArray*)aka_constraintsAffectingView:(UIView*)view;
+
+/**
+ * Gets an array containing all constraints which directly affect any of the specified views.
+ *
+ * @param views an array of views
+ *
+ * @return All constraints which directly affect one or more of the specified views.
+ */
+- (NSArray*)aka_constraintsAffectingViews:(NSArray*)views;
+
+/**
+ * Gets all constraints which only (directly) affect this instance
+ *
+ * @sa aka_isTheOnlyViewDirectlyAffectedBy:
+ *
+ * @return All constraints which do not (directly) affect other views.
+ */
+- (NSArray*)aka_constraintsAffectingOnlySelf;
+
+/**
+ * Removes all constraints of this instance, which (directly) affect the specified view.
+ *
+ * @param view the view
+ *
+ * @return an array containing the removed constraints.
+ */
+- (NSArray*)aka_removeConstraintsAffectingView:(UIView*)view;
+
+/**
+ * Removes all constraints of this instance, which (directly) affect any of the specified views.
+ *
+ * @param views the views
+ *
+ * @return an array containing the removed constraints.
+ */
+- (NSArray *)aka_removeConstraintsAffectingViews:(NSArray *)views;
+
+/**
+ * Removes all constraints of this instance, which do not (directly) affect any views except this instance.
+ *
+ * @return an array containing the removed constraints.
+ */
+- (NSArray *)aka_removeConstraintsAffectingOnlySelf;
+
+#pragma mark - Themes
+
+/**
+ * Applies the theme specification to the subviews specified in views.
+ *
+ * A theme specification consists of two toplevel elements: The @b viewCustomization,
+ * which allows you to overwrite UIView properties (using KVC) and the @b layouts,
+ * containing conditional constraint specifications to apply.
+ *
+ * For example:
+ * @code
+ * @{ @"metrics": @{ @"pad": @4, @"c1Width": @100, @"hspace": @4 },
+ *    @"viewCustomization":
+ *    @{ @"label":     @{ @"textFont":  [UIFont systemFontOfSize:14.0] },
+ *       @"textField": @{ @"textFont":  [UIFont systemFontOfSize:14.0] },
+ *       @"error":     @{ @"textFont":  [UIFont systemFontOfSize:10.0],
+ *                        @"textColor": [UIColor redColor]             },
+ *     },
+ *    @"layouts":
+ *    @[ @{ @"requiredViews": @[ @"label", @"textField", @"error" ],
+ *          @"metrics": @{ @"vspace": @2 },
+ *          @"constraints":
+ *          @[ @{ @"format": @"H:|-(pad)-[label(c1Width)]-(hspace)-[textField]-(pad)-|",
+ *                @options": @(NSLayoutFormatAlignAllFirstBaseline)
+ *              },
+ *             @{ @"format": @"V:|-(pad)-[textField]-(vspace)-[error]-(pad)-|",
+ *                @"options": @(NSLayoutFormatAlignAllLeft|NSLayoutFormatAlignAllRight)}
+ *           ],
+ *        },
+ *       @{ @"requiredViews" @[ @"label", @"textField" ],
+ *          @"constraints":
+ *          @[ @{ @"format": @"H:|-(pad)-[label(c1Width)]-(hspace)-[textField]-(pad)-|",
+ *                @options": @()
+ *              },
+ *             @{ @"format": @"V:|-(pad)-[textField]-(pad)-|" }
+ *           ]
+ *        }
+ *     ]
+ * @endcode
+ * defines a theme applicable to a view (self) which contains two or three subviews identified
+ * by names "label", "textField" and "error". If applied, the theme will change the properties
+ * textFont and textColor as specified in viewCustomization and use the first applicable
+ * constraint specification. A constraint specification is applicable if all required views
+ * are found in the @b views dictionary.
+ *
+ * @ref aka_constraintsForSpecification:views:metrics: for a documentation of the
+ * constraint specification format.
+ *
+ * @param themeSpecification the theme specification (see description)
+ * @param views a dictionary mapping view names to names.
+ *
+ * @return @{ @"addedConstraints": @[...], @"removedConstraints": @[...] }
+ */
+- (NSDictionary*)aka_applyTheme:(NSDictionary*)themeSpecification
+                        toViews:(NSDictionary*)views;
+
+/**
+ * Generates constraints based on the specification which can be
+ * provided as visual format with options:
+ * @code
+ * @{ @"format": @"|-4-[view1]-4-[view2]-4-|",\n
+ *    @"options": @(NSLayoutFormatAlignAllFirstBaseline)
+ *    }
+ * @endcode
+ * plain constraint properties:
+ * @code
+ * @{ @"firstItem": @"view1",
+ *    @"firstAttribute": @(NSLayoutAttributeBottom),
+ *    @"relatedBy": @"==",
+ *    @"secondItem": @"view2",
+ *    @"secondAttribute": @(NSLayoutAttributeBottom),
+ *    @"multiplier": @(1.1),
+ *    @"constant": @(4),
+ *    @"priority": @(1000)
+ *    }
+ * @endcode
+ * or constraint sets (where either or both of firstItem and secondItem
+ * can be replaced by firstItems or secondItems:
+ * @code
+ * @{ @"firstItem": @"view1",
+ *    @"firstAttribute": @(NSLayoutAttributeBottom),
+ *    @"relatedBy": @"<=",
+ *    @"secondItems": @[ @{ @"item": @"view2" },
+ *                       @{ @"item": @"view3",
+ *                          @"relatedBy": @(NSLayoutRelationLessThanOrEqual)
+ *                          @"secondAttribute": @(NSLayoutAttributeTop),
+ *                          @"constant" @(2) }
+ *                       ],
+ *    @"secondAttribute": @(NSLayoutAttributeBottom),
+ *    @"multiplier": @(1.1),
+ *    @"constant": @(4),
+ *    @"priority": @(1000)
+ *    }
+ * @endcode
+ * Please note that properties defined in firstItems and secondItems
+ * override those defined at toplevel. It is invalid to specify
+ * the same property in both firstItems and secondItems entries.
+ *
+ * @note You should use the visual format in preference to plain constraint specifications.
+ *
+ * @param specification a constraint specification (see description)
+ * @param views a dictionary mapping names to views
+ * @param metrics a dictionary mapping names to metric values
+ *
+ * @return an array contains the generated constraints.
+ */
+- (NSArray*)aka_constraintsForSpecification:(NSDictionary*)specification
+                                      views:(NSDictionary*)views
+                                    metrics:(NSDictionary*)metrics;
 
 @end
--- AKACommons/Classes/UIView+AKAConstraintTools.m
+++ AKACommons/Classes/UIView+AKAConstraintTools.m
@@ -10,22 +10,339 @@
 
 @implementation UIView (AKAConstraintTools)
 
-- (NSArray *)aka_removeConstraintsAffecting:(UIView *)view
+- (BOOL)aka_isAffectedByConstraint:(NSLayoutConstraint*)constraint
 {
-    NSMutableArray* constraintsToRemove = NSMutableArray.new;
+    return constraint.firstItem == self || constraint.secondItem == self;
+}
 
+- (BOOL)aka_isTheOnlyViewDirectlyAffectedBy:(NSLayoutConstraint*)constraint
+{
+    return (constraint.firstItem == self && \
+            constraint.class == [NSLayoutConstraint class] && \
+            (constraint.secondItem == self || constraint.secondItem == nil));
+}
+
+- (NSArray*)aka_constraintsAffectingView:(UIView*)view
+{
+    NSMutableArray* result = NSMutableArray.new;
     for (NSLayoutConstraint* constraint in self.constraints)
     {
-        if (constraint.firstItem == view || constraint.secondItem == view)
+        if ([view aka_isAffectedByConstraint:constraint])
         {
-            [constraintsToRemove addObject:constraint];
+            [result addObject:constraint];
         }
     }
-    if (constraintsToRemove.count > 0)
+    return result;
+}
+
+- (NSArray*)aka_constraintsAffectingOnlySelf
+{
+    NSMutableArray* result = NSMutableArray.new;
+    for (NSLayoutConstraint* constraint in self.constraints)
     {
-        [self removeConstraints:constraintsToRemove];
+        if ([self aka_isTheOnlyViewDirectlyAffectedBy:constraint])
+        {
+            [result addObject:constraint];
+        }
     }
+    return result;
+}
+
+- (NSArray*)aka_constraintsAffectingViews:(NSArray*)views
+{
+    NSMutableArray* result = NSMutableArray.new;
+    for (NSLayoutConstraint* constraint in self.constraints)
+    {
+        for (UIView* view in views)
+        {
+            if ([view aka_isAffectedByConstraint:constraint])
+            {
+                [result addObject:constraint];
+            }
+        }
+    }
+    return result;
+}
+
+- (NSArray *)aka_removeConstraintsAffectingOnlySelf
+{
+    NSArray* constraintsToRemove = [self aka_constraintsAffectingOnlySelf];
+    [self removeConstraints:constraintsToRemove];
     return constraintsToRemove;
+}
+
+- (NSArray *)aka_removeConstraintsAffectingView:(UIView *)view
+{
+    NSArray* constraintsToRemove = [self aka_constraintsAffectingView:view];
+    [self removeConstraints:constraintsToRemove];
+    return constraintsToRemove;
+}
+
+- (NSArray *)aka_removeConstraintsAffectingViews:(NSArray *)views
+{
+    NSArray* constraintsToRemove =  [self aka_constraintsAffectingViews:views];
+    [self removeConstraints:constraintsToRemove];
+    return constraintsToRemove;
+}
+
+#pragma mark - Themes
+
+- (NSDictionary*)aka_applyTheme:(NSDictionary*)themeSpecification
+               toViews:(NSDictionary*)views
+{
+    NSMutableArray* removedConstraints = NSMutableArray.new;
+    NSMutableArray* addedConstraints = NSMutableArray.new;
+
+    NSDictionary* viewCustomization = themeSpecification[@"viewCustomization"];
+    for (NSString* viewName in views.keyEnumerator)
+    {
+        UIView* view = views[viewName];
+        if (view != nil)
+        {
+            NSDictionary* customization = viewCustomization[viewName];
+            if (customization != nil)
+            {
+                for (NSString* property in customization.keyEnumerator)
+                {
+                    id value = customization[property];
+                    [view setValue:value forKey:property];
+                }
+            }
+        }
+    }
+
+    NSDictionary* defaultMetrics = themeSpecification[@"metrics"];
+    NSArray* layouts = themeSpecification[@"layouts"];
+    for (NSDictionary* layout in layouts)
+    {
+        BOOL applicable = YES;
+        NSArray* requiredViews = layout[@"requiredViews"];
+        for (NSString* viewName in requiredViews)
+        {
+            if (views[viewName] == nil)
+            {
+                applicable = NO;
+                break;
+            }
+        }
+
+        if (applicable)
+        {
+            NSDictionary* metrics = layout[@"metrics"];
+            if (metrics == nil)
+            {
+                metrics = defaultMetrics;
+            }
+            else
+            {
+                NSMutableDictionary* tmp = [NSMutableDictionary dictionaryWithDictionary:defaultMetrics];
+                for (NSString* key in metrics)
+                {
+                    tmp[key] = metrics[key];
+                }
+                metrics = tmp;
+            }
+            for (UIView* view in views.objectEnumerator)
+            {
+                // TODO: record target from which constraints have been removed:
+                [removedConstraints addObjectsFromArray:[self aka_removeConstraintsAffectingView:view]];
+                [removedConstraints addObjectsFromArray:[view aka_removeConstraintsAffectingView:self]];
+                [removedConstraints addObjectsFromArray:[view aka_removeConstraintsAffectingOnlySelf]];
+            }
+            for (NSDictionary* constraintSpec in layout[@"constraints"])
+            {
+                NSArray* constraints = [self aka_constraintsForSpecification:constraintSpec
+                                                                       views:views
+                                                                     metrics:metrics];
+                [self addConstraints:constraints];
+                [addedConstraints addObjectsFromArray:constraints];
+            }
+
+            break;
+        }
+    }
+    return NSDictionaryOfVariableBindings(addedConstraints, removedConstraints);
+}
+
+- (NSArray*)aka_constraintsForSpecification:(NSDictionary*)specification
+                                      views:(NSDictionary*)views
+                                    metrics:(NSDictionary*)metrics
+{
+    NSArray* result = nil;
+    NSString* visualFormat = specification[@"format"];
+    if (visualFormat)
+    {
+        NSLayoutFormatOptions options = ((NSNumber*)specification[@"options"]).unsignedIntegerValue;
+
+        result = [NSLayoutConstraint constraintsWithVisualFormat:visualFormat
+                                                         options:options
+                                                         metrics:metrics
+                                                           views:views];
+    }
+    else
+    {
+        NSMutableArray* constraints = NSMutableArray.new;
+
+        NSArray* firstItems = specification[@"firstItems"];
+        if (firstItems == nil)
+        {
+            NSString* item = specification[@"firstItem"];
+            if (item)
+            {
+                firstItems = @[ @{ @"item": item } ];
+            }
+        }
+        NSArray* secondItems = specification[@"secondItems"];
+        if (secondItems == nil)
+        {
+            NSString* item = specification[@"secondItem"];
+            if (item)
+            {
+                secondItems = @[ @{ @"item": item } ];
+            }
+        }
+
+        for (NSDictionary* firstItemSpec in firstItems)
+        {
+            for (NSDictionary* secondItemSpec in secondItems)
+            {
+                UIView* firstItem = [self resolveConstraintItem:firstItemSpec[@"item"]
+                                                        inViews:views];
+                NSNumber* firstAttribute = [self resolveKey:@"firstAttribute"
+                                                      inTop:specification
+                                                      first:firstItemSpec
+                                                     second:secondItemSpec];
+                UIView* secondItem = [self resolveConstraintItem:secondItemSpec[@"item"]
+                                                         inViews:views];
+                NSNumber* secondAttribute = [self resolveKey:@"secondAttribute"
+                                                      inTop:specification
+                                                      first:firstItemSpec
+                                                     second:secondItemSpec];
+                NSNumber* relatedBy = [self resolveKey:@"secondAttribute"
+                                                        inTop:specification
+                                                        first:firstItemSpec
+                                                       second:secondItemSpec];
+                relatedBy = @([self resolveConstraintRelation:relatedBy]);
+                NSNumber* constant = [self resolveKey:@"constant"
+                                                inTop:specification first:firstItemSpec second:secondItemSpec];
+                constant = [self resolveConstraintValue:constant
+                                              inMetrics:metrics];
+                NSNumber* multiplier = [self resolveKey:@"multiplier"
+                                                  inTop:specification first:firstItemSpec second:secondItemSpec];
+                if (multiplier == nil)
+                {
+                    multiplier = @1;
+                }
+                NSNumber* priority = [self resolveKey:@"priority"
+                                               inTop:specification
+                                               first:firstItemSpec
+                                              second:secondItemSpec];
+                priority = [self resolveConstraintValue:priority
+                                             inMetrics:metrics];
+
+                NSLayoutConstraint* constraint =
+                    [NSLayoutConstraint constraintWithItem:firstItem
+                                                 attribute:firstAttribute.intValue
+                                                 relatedBy:relatedBy.intValue
+                                                    toItem:secondItem
+                                                 attribute:secondAttribute.intValue
+                                                multiplier:multiplier.doubleValue
+                                                  constant:constant.doubleValue];
+                if ([priority isKindOfClass:[NSNumber class]])
+                {
+                    constraint.priority = ((NSNumber*)priority).intValue;
+                }
+                [constraints addObject:constraint];
+
+            }
+        }
+
+        if (constraints.count > 0)
+        {
+            result = constraints;
+        }
+    }
+    return result;
+}
+
+- (id)resolveKey:(NSString*)key
+                  inTop:(NSDictionary*)toplevel
+                  first:(NSDictionary*)firstItemSpec
+                 second:(NSDictionary*)secondItemSpec
+{
+    NSParameterAssert(firstItemSpec[key] == nil || secondItemSpec[key] == nil);
+
+    id result = toplevel[key];
+    id firstValue = firstItemSpec[key];
+    id secondValue = secondItemSpec[key];
+
+    if (firstValue != nil)
+    {
+        result = firstValue;
+    }
+    else if (secondValue != nil)
+    {
+        result = secondValue;
+    }
+    return result;
+}
+
+
+- (NSLayoutRelation)resolveConstraintRelation:(id)relation
+{
+    NSLayoutRelation result = NSLayoutRelationEqual;
+    if ([relation isKindOfClass:[NSString class]])
+    {
+        if ([@"<=" isEqualToString:relation])
+        {
+            result = NSLayoutRelationLessThanOrEqual;
+        }
+        else if ([@">=" isEqualToString:relation])
+        {
+            result = NSLayoutRelationGreaterThanOrEqual;
+        }
+        else if (![@"==" isEqualToString:relation])
+        {
+            // TODO: error handling
+        }
+    }
+    else if ([relation isKindOfClass:[NSNumber class]])
+    {
+        result = ((NSNumber*)relation).unsignedIntegerValue;
+    }
+    else
+    {
+        // TODO: error handling
+    }
+    return result;
+}
+
+- (UIView*)resolveConstraintItem:(id)item inViews:(NSDictionary*)views
+{
+    UIView* result = nil;
+    if ([item isKindOfClass:[UIView class]])
+    {
+        result = item;
+    }
+    else
+    {
+        result = views[item];
+    }
+    return result;
+}
+
+- (NSNumber*)resolveConstraintValue:(id)value inMetrics:(NSDictionary*)metrics
+{
+    NSNumber* result;
+    if ([value isKindOfClass:[NSNumber class]])
+    {
+        result = value;
+    }
+    else
+    {
+        result = metrics[value];
+    }
+    return result;
 }
 
 @end

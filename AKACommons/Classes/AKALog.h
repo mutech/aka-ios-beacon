f3bb0529d1000ac33fbe48e69656b64023976cf3
Michael Utech <michael.utech@aka-labs.com>
2015-09-29 05:25:40 +0200

    wip: Keyboard activation sequence, refactoring control interfaces, code cleanup, workspace/project structure cleanup

--- AKACommons/Classes/AKALog.h
+++ AKACommons/Classes/AKALog.h
@@ -6,7 +6,9 @@
 //  Copyright (c) 2015 AKA Sarl. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+
+// TODO: use DDLog with separate logLeve scope
 
 // Implementation notes: do/while to make it a statement requiring a semicolon; ## to remove the
 // preceeding comma if no variadic args are given.
@@ -14,10 +16,11 @@
     do{ NSString* msg = [[level stringByAppendingString:@": "] stringByAppendingString:[NSString stringWithFormat:(format),##__VA_ARGS__]];\
         NSLog(@"%@",msg); } while(0)
 
-#define AKALogDebug(format, ...)  AKALogLevel(@"DEBUG", (format),##__VA_ARGS__)
-#define AKALogInfo(format, ...)   AKALogLevel(@"INFO",  (format),##__VA_ARGS__)
+#define AKALogDebug(format, ...)  AKALogLevel(@"DEBUG", (format), ##__VA_ARGS__)
+#define AKALogInfo(format, ...)   AKALogLevel(@"INFO",  (format), ##__VA_ARGS__)
 #define AKALogWarn(format, ...)   AKALogLevel(@"WARN",  (format), ##__VA_ARGS__)
 #define AKALogError(format, ...)  AKALogLevel(@"ERROR", (format), ##__VA_ARGS__)
+#define AKALogTrace(format, ...)  AKALogLevel(@"TRACE", (format), ##__VA_ARGS__)
 
 @interface AKALog : NSObject
 
